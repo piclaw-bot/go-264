31c92c27f53243ce1248f3b013c8a648cc640384
Rui Carmo <rui.carmo@gmail.com>
2026-05-19 11:12:29 +0100

    Tools: compare B8x8 representative cache MVs

--- decode/reconstruct_inter.go
+++ decode/reconstruct_inter.go
@@ -714,11 +714,12 @@ func (d *Decoder) reconstructMBBidi(f *frame.Frame, mb *syntax.MBBidi, mbX, mbY,
 	if os.Getenv("GO264_B_MB_TRACE") != "" {
 		sub0, sub1, sub2, sub3 := directTraceSubTypes(mb)
 		smv0, smv1, smv2, smv3 := directTraceSubMVs(mb)
+		p0L0, p0L1 := bTracePart0MVs(mb)
 		p1L0, p1L1 := bTracePart1MVs(mb)
 		fmt.Fprintf(os.Stderr,
 			"GOBIDI mb=%04d x=%02d y=%02d poc=%d mb_type=%d ref0=%d ref1=%d mv0={%d,%d} mv1={%d,%d} mv0p1={%d,%d} mv1p1={%d,%d} cbp=%02x qpd=%d sub0=%d sub1=%d sub2=%d sub3=%d submv0={%d,%d} submv1={%d,%d} submv2={%d,%d} submv3={%d,%d}\n",
 			mbY*d.mbW+mbX, mbX, mbY, f.POC, mb.MBType,
-			mb.RefIdxL0[0], mb.RefIdxL1[0], mb.MVL0[0].X, mb.MVL0[0].Y, mb.MVL1[0].X, mb.MVL1[0].Y,
+			mb.RefIdxL0[0], mb.RefIdxL1[0], p0L0.X, p0L0.Y, p0L1.X, p0L1.Y,
 			p1L0.X, p1L0.Y, p1L1.X, p1L1.Y,
 			mb.CBP, mb.QPDelta, sub0, sub1, sub2, sub3,
 			smv0.X, smv0.Y, smv1.X, smv1.Y, smv2.X, smv2.Y, smv3.X, smv3.Y)
@@ -802,6 +803,16 @@ func directTraceSubType(t uint32) uint32 {
 		return ffBSubType[t]
 	}
 	return t
+}
+
+func bTracePart0MVs(mb *syntax.MBBidi) (syntax.MotionVector, syntax.MotionVector) {
+	if mb == nil {
+		return syntax.MotionVector{}, syntax.MotionVector{}
+	}
+	if mb.MBType == syntax.BMBTypeB8x8 {
+		return mb.SubMVL0[0], mb.SubMVL1[0]
+	}
+	return mb.MVL0[0], mb.MVL1[0]
 }
 
 func bTracePart1MVs(mb *syntax.MBBidi) (syntax.MotionVector, syntax.MotionVector) {
